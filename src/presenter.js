41bee0bf19e9f5a87752f2debbea8d470bda0446
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-03-05 17:13:35 -0400

    Base de datos para la lista de notas, añadida en el presenter.js para implementar el buscador

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador";
+// import lista_notas from ".\lista_notas";
 
 const titulo = document.querySelector("#titulo");
 const descripcion = document.querySelector("#descripcion");
@@ -6,13 +6,16 @@ const form = document.querySelector("#nota-form");
 const div = document.querySelector("#resultado_nota_div");
 const ul = document.querySelector('ul');
 
+let LN = new Map();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  LN.set(titulo, descripcion);
 
   const li = document.createElement("li");
   const p = document.createElement("p");
   p.innerHTML = "<p>" + "<strong>" + titulo.value + "</strong>" + "<br>"+ descripcion.value +"</p>";
+  
   li.appendChild(p);
   ul.appendChild(li);
-
 });
