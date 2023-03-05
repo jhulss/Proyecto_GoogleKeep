4fbd66e6caecd4593333f8063842f84dcfde4966
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-03-05 16:29:56 -0400

    Ajustes en el presenter.js para añadir lista de nota. Primeras ajustes en ello

--- src/presenter.js
+++ src/presenter.js
@@ -4,9 +4,15 @@ const titulo = document.querySelector("#titulo");
 const descripcion = document.querySelector("#descripcion");
 const form = document.querySelector("#nota-form");
 const div = document.querySelector("#resultado_nota_div");
+const ul = document.querySelector('ul');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + "<strong>" + titulo.value + "</strong>" + "<br>"+ descripcion.value +"</p>";
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  p.innerHTML = "<p>" + "<strong>" + titulo.value + "</strong>" + "<br>"+ descripcion.value +"</p>";
+  li.appendChild(p);
+  ul.appendChild(li);
+
 });
